9c1e864a861eea17f0e65863502c6ca16aaf255f
Finn <finn@finn.io>
2016-05-31 20:42:36 -0700

    Add a deleterecording script to clean up old recordings

---
+++ DeleteOldRecordings/delete-old-recordings.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import ConfigParser
+import MySQLdb
+import os
+
+
+parser = ConfigParser.ConfigParser()
+parser.read("/etc/rd.conf")
+
+
+dbopts = {'Hostname': 'localhost', 'Loginname': 'rduser', 'Password': None, 'Database': 'Rivendell'}
+if parser.has_option("mySQL", "Hostname"):
+    dbopts['Hostname'] = parser.get("mySQL", "Hostname")
+if parser.has_option("mySQL", "Loginname"):
+    dbopts['Loginname'] = parser.get("mySQL", "Loginname")
+    if parser.has_option("mySQL", "Password"):
+        dbopts['Password'] = parser.get("mySQL", "Password")
+if parser.has_option("mySQL", "Database"):
+    dbopts['Database'] = parser.get("mySQL", "Database")
+
+db = MySQLdb.connect(dbopts['Hostname'], dbopts['Loginname'], dbopts['Password'],
+                     dbopts['Database'])
+cursor = db.cursor()
+
+conditions = "FROM CUTS WHERE ORIGIN_DATETIME < DATE_SUB(NOW(), INTERVAL 2 YEAR) AND CART_NUMBER BETWEEN 80000 AND 90000;"
+
+cursor.execute("SELECT CUT_NAME %s" % conditions)
+for result in cursor.fetchall():
+    os.remove("/var/snd/%s.wav" % result[0])
+
+cursor.execute("DELETE %s" % conditions)
