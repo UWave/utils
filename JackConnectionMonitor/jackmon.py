2374b99ba5163c5d9ee3250771f43ead3ea0df77
Finn <finn@uwave.fm>
2016-07-14 12:09:39 -0700

    logging

--- JackConnectionMonitor/jackmon.py
+++ JackConnectionMonitor/jackmon.py
@@ -3,12 +3,23 @@
 import jack
 import os
 import json
+import logging
+import sys
+
+
+if "-d" in sys.argv:
+    logging.basicConfig(level=logging.DEBUG)
 
 client = jack.Client(os.path.basename(__file__))
 conf = json.load(open("/etc/jackmon.conf"))
 
 for p in conf['ports']:
     try:
-        client.connect(p[0], p[1])
+        connections = client.get_all_connections(p[0])
+        if p[1] not in connections:
+            logging.warning("%s wasn't connected to %s! I've connected them", p[0], p[1])
+            client.connect(p[0], p[1])
+        else:
+            logging.debug("%s already connected to %s", p[0], p[1])
     except jack.Error as e:
-        print("Error: {} for ports {} and {}".format(e, p[0], p[1]))
+        logging.error("Error: {} for ports {} and {}".format(e, p[0], p[1]))
